08049cceb514b11112e895fd3cf4f683e0d30b26
Michal Nedzka <michael.nedzka@gmail.com>
2018-09-18 22:41:18 +0200

    Add prettier, loop over cities and add them to UI on button click

--- src/containers/SearchBar.js
+++ src/containers/SearchBar.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react'
-import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
 import { fetchWeather } from "../actions/index";
 
 class SearchBar extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
-      term: '',
-    }
+      term: ""
+    };
 
     this.onInputChange = this.onInputChange.bind(this);
     this.formOnSubmit = this.formOnSubmit.bind(this);
@@ -17,37 +17,42 @@ class SearchBar extends Component {
 
   onInputChange(event) {
     this.setState({
-      term: event.target.value,
-    })
-    console.log(this.state)
-  };
+      term: event.target.value
+    });
+    console.log(this.state);
+  }
 
   formOnSubmit(event) {
     event.preventDefault();
 
     this.props.fetchWeather(this.state.term);
-    this.setState({term: ''})
+    this.setState({ term: "" });
   }
 
   render() {
     return (
       <form onSubmit={this.formOnSubmit} className="input-group">
-          <input 
-            placeholder="Get a five day forecast in your favorite city"
-            className="form-control"
-            value={this.state.term}
-            onChange={this.onInputChange}
-          />
-          <span className="input-group-button">
-            <button type="submit" className="btn button-secondary">Submit</button>
-          </span>
+        <input
+          placeholder="Get a five day forecast in your favorite city"
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChange}
+        />
+        <span className="input-group-button">
+          <button type="submit" className="btn button-secondary">
+            Submit
+          </button>
+        </span>
       </form>
-    )
+    );
   }
 }
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators({ fetchWeather }, dispatch)
+  return bindActionCreators({ fetchWeather }, dispatch);
 }
 
-export default connect(null, mapDispatchToProps)(SearchBar)
+export default connect(
+  null,
+  mapDispatchToProps
+)(SearchBar);
--- src/containers/WeatherList.js
+++ src/containers/WeatherList.js
@@ -1,28 +1,37 @@
-import React, { Component } from 'react';
-import {connect} from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class WeatherList extends Component {
+  renderWeather(cityData) {
+    const name = cityData.city.name;
+    return (
+      <tr key={name}>
+        <td>{name}</td>
+      </tr>
+    );
+  }
+
   render() {
     return (
       <div>
         <table className="table table-hover">
-            <thead>
-                <tr>
-                    <th>City</th>
-                    <th>Temperature</th>
-                    <th>Pressure</th>
-                    <th>Humidity</th>
-                </tr>
-            </thead>
-            <tbody></tbody>
+          <thead>
+            <tr>
+              <th>City</th>
+              <th>Temperature</th>
+              <th>Pressure</th>
+              <th>Humidity</th>
+            </tr>
+          </thead>
+          <tbody>{this.props.weather.map(this.renderWeather)}</tbody>
         </table>
       </div>
-    )
+    );
   }
 }
 
-function mapStateToProps({weather}) {
-    return { weather }
+function mapStateToProps({ weather }) {
+  return { weather };
 }
 
 export default connect(mapStateToProps)(WeatherList);
