0c01679ffdac556c32752cc5fbfa965d81f456b9
Michal Nedzka <michael.nedzka@gmail.com>
2018-09-19 23:34:09 +0200

    Add small fixes

--- src/components/googleMap.js
+++ src/components/googleMap.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 export default class GoogleMap extends Component {
   componentDidMount() {
-    new GoogleMap.maps.Map(this.ref.map, {
+    new google.maps.Map(this.refs.map, {
       zoom: 12,
       center: {
         lat: this.props.lat,
